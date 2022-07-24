4efad1b56df82ab68d707babcb1898348845be91
opteacher <opteacher@hotmail.com>
2022-07-24 12:51:23 +0800

    表项卡应能应对数据为空的情况

--- src/components/table/CellCard.vue
+++ src/components/table/CellCard.vue
@@ -4,8 +4,8 @@
       color: selected ? '#1890ff' : cell.color
     }"
   >
-    {{ cell.prefix && !strTxt.startsWith(cell.prefix) ? cell.prefix : '' }}{{ strTxt
-    }}{{ cell.suffix && !endsWith(strTxt, cell.suffix) ? cell.suffix : '' }}
+    {{ cell.prefix && !text.startsWith(cell.prefix) ? cell.prefix : '' }}{{ text
+    }}{{ cell.suffix && !endsWith(text, cell.suffix) ? cell.suffix : '' }}
   </span>
 </template>
 
@@ -17,13 +17,11 @@ export default defineComponent({
   name: 'CellCard',
   props: {
     cell: { type: Object, required: true },
-    text: { type: Object, required: true },
+    text: { type: String, required: true },
     selected: { type: Boolean, default: false }
   },
-  setup(props) {
-    const strTxt = computed(() => (props.text ? props.text.toString() : ''))
+  setup() {
     return {
-      strTxt,
       endsWith
     }
   }
