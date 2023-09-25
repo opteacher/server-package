5a5fbd90f783c0e241cb1abe9adf59c9c43f2b68
opteacher <opteacher@hotmail.com>
2023-09-25 15:34:41 +0800

    修正组件模型结构（进一步）

--- server/models/compo.js
+++ server/models/compo.js
@@ -6,18 +6,21 @@ export default db.defineModel(
     name: db.PropTypes.String,
     ctype: db.PropTypes.String, // String-input/Number-Number/String-Select/Array-Cascader/String-Textarea/Boolean|Array-Checkbox/Boolean-Switch/String-Radio/Array-List/String-Color/String-Icon
     category: db.PropTypes.String, // field/display/container
+    inner: db.PropTypes.String,
     props: [
       {
         label: db.PropTypes.String,
         desc: db.PropTypes.String,
         ftype: db.PropTypes.String, // 参考CompoType
+        vtype: db.PropTypes.String,
+        default: db.PropTypes.Any,
         rules: db.PropTypes.Array,
         refer: db.PropTypes.String, // 关联字段或字段的再处理，当然也可以为空（为单字段时，该组件与字段双向绑定）
         placeholder: db.PropTypes.String,
+        vModel: db.PropTypes.Boolean,
         extra: db.PropTypes.Object
       }
     ],
-    inner: db.PropTypes.String,
     components: [{ type: db.PropTypes.Id, ref: 'component' }]
   },
   {
