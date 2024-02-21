5ecd90bda5c3c5c7e403c796070a5e4858b7119d
opteacher <opteacher@hotmail.com>
2024-02-21 10:03:22 +0800

    修正app-temp的service多个局变量所引起的错误

--- server/resources/app-temp/services/temp.js
+++ server/resources/app-temp/services/temp.js
@@ -1,5 +1,5 @@
 /*return Object.values(Object.fromEntries(services.map(svc => svc.deps).filter(deps => deps).flat().map(dep => [dep.name, dep]))).map(dep => `import ${dep.default ? dep.exports[0] : ('{ ' + dep.exports.join(', ') + ' }')} from '${dep.from}'`).join('\n')*/
 
-/*return (stcVars || []).map(v => `// ${v.remark}\nvar ${v.name} = ${genDefault(v.vtype, v.default)}\n`)*/
+/*return (stcVars || []).map(v => `// ${v.remark}\nvar ${v.name} = ${genDefault(v.vtype, v.default)}\n`).join('\n')*/
 
 /*return services.map(svc => `export async function ${svc.interface} (ctx) {\n  try {\n${svc.codes}\n  } catch (e) {\n    console.error(e)\n    return { error: e.message || JSON.stringify(e) }\n  }\n}`).join('\n')*/
