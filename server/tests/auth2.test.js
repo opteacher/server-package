0e4d5f2c55d62bc0b93c619782d221edd5e28101
opteacher <opteacher@hotmail.com>
2024-04-19 01:56:57 +0800

    推进casbin权限控制测试

--- server/tests/auth2.test.js
+++ server/tests/auth2.test.js
@@ -1,15 +1,17 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import { beforeAll, beforeEach, afterAll, expect, test, jest } from '@jest/globals'
-process.env.NODE_ENV = 'dev'
-import { db2StrPolicy } from '../services/auth.js'
-import { db } from '../utils/index.js'
 
 describe('# 权限系统，用Casbin重写', () => {
   describe('# 可行性测试', () => {
     test('# 现有授权策略转Casbin格式', async () => {
-      const project = await db.select(Project, { name: 'login_platform' })
-      await db2StrPolicy(project._id)
+      process.env.NODE_ENV = 'dev'
+      const { db } = await import('../utils/index.js')
+      const Project = await import('../models/project.js').then(exp => exp.default)
+      const result = await db.select(Project, { name: 'login_platform' })
+      expect(result.length).not.toBe(0)
+      const { db2StrPolicy } = await import('../services/auth.js')
+      await db2StrPolicy(result[0]._id)
     })
   })
 })
