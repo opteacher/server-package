cf287046a4e1977a43a33c6b6bda25aeae2c91ad
opteacher <opteacher@hotmail.com>
2024-04-21 00:21:16 +0800

    推进casbin权限逻辑

--- server/tests/auth2.test.js
+++ server/tests/auth2.test.js
@@ -8,9 +8,18 @@ mongoose.Promise = global.Promise
 import { v4 as uuidv4 } from 'uuid'
 
 describe('# 权限系统，用Casbin重写', () => {
+  process.env.NODE_ENV = 'dev'
+  beforeAll(async () => {
+    console.log('创建一个测试项目包含权限系统……')
+    const { db } = await import('../utils/index.js')
+    const Project = await import('../models/project.js').then(exp => exp.default)
+    const Model = await import('../models/model.js').then(exp => exp.default)
+    await db.remove(Project, { name: 'test_auth2' })
+    await db.remove(Model, { name: 'test_anth2_model' })
+  })
+
   describe('# 可行性测试', () => {
     test('# 现有授权策略转Casbin格式', async () => {
-      process.env.NODE_ENV = 'dev'
       const { db } = await import('../utils/index.js')
       const Project = await import('../models/project.js').then(exp => exp.default)
       const result = await db.select(Project, { name: 'login_platform' })
@@ -21,9 +30,9 @@ describe('# 权限系统，用Casbin重写', () => {
 
     describe('# 请求校验', () => {
       let admin = null
+      let ctx = null
 
-      beforeEach(async () => {
-        process.env.NODE_ENV = 'dev'
+      beforeAll(async () => {
         const { db } = await import('../utils/index.js')
         const Admin = await import('../models/admin.js').then(exp => exp.default)
         await db.remove(Admin, { name: 'test' })
@@ -32,24 +41,36 @@ describe('# 权限系统，用Casbin重写', () => {
           phone: '13918559376',
           password: '99a7deedf7db1952a32273974b7c96970daafba863d1467941c779f7731a94ee'
         })
+        const { getSecret } = await import('../services/auth.js')
+        const authorization =
+          'Bearer ' +
+          jwt.sign(
+            {
+              sub: admin._id,
+              aud: 'server-package',
+              iat: Date.now(),
+              jti: uuidv4(),
+              iss: `server-package/${admin.name}`,
+              exp: Date.now() + 24 * 60 * 60 * 1000 // 1 day
+            },
+            await getSecret()
+          )
+        global.jest = jest
+        ctx = createMockContext({ headers: { authorization } })
       })
 
       test('# 创建koa请求附带签名，普通校验', async () => {
-        process.env.NODE_ENV = 'dev'
-        const { getSecret } = await import('../services/auth.js')
-        const authorization = jwt.sign(
-          {
-            sub: admin._id,
-            aud: 'server-package',
-            iat: Date.now(),
-            jti: uuidv4(),
-            iss: `server-package/${admin.name}`,
-            exp: Date.now() + 24 * 60 * 60 * 1000 // 1 day
-          },
-          await getSecret()
-        )
-        const ctx = createMockContext({ headers: { authorization } })
-        console.log(ctx.header)
+        const { verify } = await import('../services/auth.js')
+        const resp = await verify(ctx)
+        expect(resp.error).toBeUndefined()
+        expect(resp.payload).toHaveProperty('sub', admin._id.toString())
+      })
+
+      test('# 创建koa请求附带签名，深层校验（Casbin校验）', async () => {
+        const { verifyDeep } = await import('../services/auth.js')
+        const resp = await verifyDeep(ctx)
+        console.log(resp)
+        expect(resp.error).toBeUndefined()
       })
     })
   })
